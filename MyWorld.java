79e6561407b2d8b6f0ddbf39f9b3540d2fe36d60
KayvonN <102381370+KayvonN@users.noreply.github.com>
2022-05-03 13:53:56 -0400

    Add files via upload
    Added new mankie

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,9 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        mankie drip = new mankie();
+        addObject(drip, getWidth() / 2, getHeight() /2);
+
     }
 }
---
+++ mankie.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class monkey here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class mankie extends Actor
+{
+    /**
+     * Act - do whatever the monkey wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(1);
+        // Add your action code here.
+    }    
+}
