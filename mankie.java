73dac866fd75eae0ae1c088f5a14c228b2295e69
KayvonN <102381370+KayvonN@users.noreply.github.com>
2022-05-26 13:55:40 -0400

    Add files via upload

--- mankie.java
+++ mankie.java
@@ -8,6 +8,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class mankie extends Actor
 {
+    GreenfootImage[] idle = new GreenfootImage[5] ;
+    
+    public mankie()
+    {
+        for(int i = 1; i < 6; i++)
+        {
+            idle[i] = new GreenfootImage("Mankie_idle.gif/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateMankie()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = imageIndex + 1 % idle.length;
+    }
+    
+    
+    
     String name;
     {
         this.name = name;
@@ -40,6 +60,8 @@ public class mankie extends Actor
             world.increaseScore();
             
         }
+        
+        animateMankie();
     }    
     
 }
